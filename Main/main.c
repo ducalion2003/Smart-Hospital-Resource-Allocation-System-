9f10060d3a2784913ca1e4a3b16e82e9d395cc02
Chamod Madushan <chamodmadushan05@gmail.com>
2026-09-11 08:47:19 +0530

    Added the lookup tables for doctor specialties and hospital wards using constant arrays

--- Main/main.c
+++ Main/main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 
+#define numberOfSpecialties 4
+#define numberOfWards 4
+
+// Doctor Specialties Data Table
+const char SpecialityNames[numberOfSpecialties][30] = {
+    "General Practice (OPD)",
+    "Paediatrics",
+    "Cardiology",
+    "Neurology"
+};
+const double BaseConsultationFee[numberOfSpecialties] = {1500.00, 2500.00, 4500.00, 5000.00};
+const int ConsultationTime[numberOfSpecialties] = {15, 20, 30, 30};
+const int DailyPatientCap[numberOfSpecialties] = {30, 20, 12, 10};
+
+
+//Hospital Wards Data Table
+const char wardNames[numberOfWards][30] = {
+    "General Ward",
+    "Paediatric Ward",
+    "Surgical Ward",
+    "ICU (Intensive Care Unit)"
+};
+const double wardRates[numberOfWards] = {3000.00, 6000.00, 12000.00, 25000.00};
+const int wardCapacities[numberOfWards] = {20, 10, 10, 5};
+
+
+//2D Bed Occupancy (0 = Available, 1 = Occupied)
+int bedOccupancy[numberOfWards][20] = {0};
+
+
+
 void displayMenu();
 
+
+// Main Function
 int main() {
     int choice;
     
@@ -31,7 +64,7 @@ int main() {
     return 0;
 }
 
-
+// Function to display the main menu
 void displayMenu() {
     printf("\n=== SMART HOSPITAL MANAGEMENT SYSTEM ===\n");
     printf("1. Register New Patient\n");
